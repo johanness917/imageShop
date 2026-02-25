1dda0dd515dd77551360fae005131c1e4fa6fa47
johanness917 <jhhan917@gmail.com>
2026-02-25 17:50:09 +0900

    [수정]  회원(등록/보기/수정/삭제) 시스템 구성, 관리 Member 테이블을 이용한 인증/인가 처리 기능 수정, 웹 페이지 생성, 로그인, 로그아웃 기능 수정, 애너테이션을 사용하여 접근 제한을 설정할 수 있도록 스프링 시큐리티 애너테이션 활성화 수정

--- src/main/java/com/project/mapper/MemberMapper.java
+++ src/main/java/com/project/mapper/MemberMapper.java
@@ -1,12 +1,35 @@
 package com.project.mapper;
 
+import java.util.List;
+
 import com.project.domain.Member;
 import com.project.domain.MemberAuth;
 
 public interface MemberMapper {
-
+	// 등록 처리
 	public int register(Member member) throws Exception;
 
+	// 권한 생성
 	public void createAuth(MemberAuth memberAuth) throws Exception;
+
+	// 목록 페이지
+	public List<Member> list() throws Exception;
+
+	// 상세 페이지
+	public Member read(Member member) throws Exception;
+	
+	public Member readByUserId(Member member) throws Exception;
+
+	// 수정 처리
+	public int modify(Member member) throws Exception;
+
+	public void deleteAuth(Member member) throws Exception;
+
+	// 권한 수정
+	public void modifyAuth(MemberAuth memberAuth) throws Exception;
+
+	public int remove(Member member) throws Exception;
+
+	public int countAll() throws Exception;
 
 }
